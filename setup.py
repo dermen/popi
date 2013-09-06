62aca0415cda88b46dae754dd390dbf8f410303a
dermen <dermen@stanford.edu>
2013-09-06 02:31:23 -0400

    added corr and popi

--- setup.py
+++ setup.py
@@ -1,6 +1,53 @@
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+try:
+    numpy_include = numpy.get_include()
+except AttributeError:
+    numpy_include = numpy.get_numpy_include()
 
-setup(name='thor',
-    version='1.0',
-    package_dir={'thor': 'src'},
-    packages=['thor'])
+corr = Extension(
+        'thor.corr',
+         sources=['src/corr/correlate.pyx', 'src/corr/corr.cpp'],
+         extra_compile_args={'gcc': ['--fast-math', '-O3', '-fPIC', '-Wall'],
+                             'g++': ['--fast-math', '-O3', '-fPIC', '-Wall']},
+         runtime_library_dirs=['/usr/lib', '/usr/local/lib'],
+         extra_link_args = ['-lstdc++', '-lm'],
+         include_dirs = [numpy_include, 'src/corr'],
+         language='c++')
+
+popi = Extension('thor.popi',
+                 sources=['src/popi/polar_pilatus.pyx', 'src/popi/popi.cpp'],
+                 extra_compile_args={'gcc': ['--fast-math', '-O3', '-fPIC', '-Wall'],
+                                     'g++': ['--fast-math', '-O3', '-fPIC', '-Wall']},
+                 runtime_library_dirs=['/usr/lib', '/usr/local/lib'],
+                 extra_link_args = ['-lstdc++', '-lm'],
+                 include_dirs = [numpy_include, 'src/popi'],
+                 language='c++')
+
+def compiler_(self):
+    default_compiler_so = self.compiler_so
+    super = self._compile
+
+    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+        postargs = extra_postargs['gcc']
+        super(obj, src, ext, cc_args, postargs, pp_opts)
+        self.compiler_so = default_compiler_so
+
+    self._compile = _compile
+
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        compiler_(self.compiler)
+        build_ext.build_extensions(self)
+
+setup(  name        ='thor',
+        version     ='1.0',
+        cmdclass   = {'build_ext': custom_build_ext },
+        ext_modules = [corr,popi],
+        package_dir = {'thor': 'src'},
+        packages    = ['thor']  )
+
+
+
---
+++ src/corr/correlate.pyx
@@ -0,0 +1,65 @@
+import numpy as np
+cimport numpy as np
+import random
+from scipy import fftpack
+
+cdef extern from "corr.h":
+  cdef cppclass Corr:
+    Corr(int N_, float * ar1, float * ar2, float * ar3) except +
+
+cdef Corr * c
+
+def correlate(A,B):
+  """
+  compute the correlation between 2 arrays
+  Parameters:
+  -----------
+  A, 1D numpy array
+  B, 1D numpy array 
+  """
+  if A.shape != B.shape:
+    print "arrays must be of same size and shape" 
+    return 0  # will add an traceback once I learn how :)
+  N = A.shape[0]
+  C = np.zeros_like(A)
+  cdef np.ndarray[ndim=1,dtype=np.float32_t] v1
+  cdef np.ndarray[ndim=1,dtype=np.float32_t] v2
+  cdef np.ndarray[ndim=1,dtype=np.float32_t] v3
+  
+  v1 = np.ascontiguousarray(A.flatten(),dtype=np.float32)
+  v2 = np.ascontiguousarray(B.flatten(),dtype=np.float32)
+  v3 = np.ascontiguousarray(C.flatten(),dtype=np.float32)
+  c  = new Corr(N,&v1[0], &v2[0], &v3[0])
+  del c
+  return v3
+
+def correlate_using_fft(self, x, y):
+  """
+  Compute the correlation between 2 arrays using the 
+  convolution theorem. Works well for unmasked arrays.
+  Passing masked arrays will results in numerical errors.
+
+  Parameters
+  ----------
+  x : 1d numpy array of floats
+      The intensities along ring 1
+  y : 1d numpy array of floats
+      The intensities along ring 2
+    
+  Returns
+  -------
+  iff : 1d numpy darray, float
+      The correlation between x and y
+  """
+
+  xmean = x.mean()
+  ymean = y.mean()
+
+  # use d-FFT + convolution thm
+  ffx = fftpack.fft( x-xmean )
+  ffy = fftpack.fft( y-ymean )
+  iff = np.real( fftpack.ifft( np.conjugate(ffx) * ffy ) )
+
+  return iff / float ( x.shape[0] )
+
+
